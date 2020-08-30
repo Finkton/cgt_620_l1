c8f23518a520079a2e94dc66a1864f63a776fc78
Finkton <saad.jmal@gmail.com>
2020-08-30 17:43:05 -0400

    simple multi processing to find max

--- cgt_620_l1/main.cpp
+++ cgt_620_l1/main.cpp
@@ -6,11 +6,62 @@
 //  Copyright © 2020 Saad Jamal. All rights reserved.
 //
 
-#include <iostream>
-#include <omp.h>
+#include <stdio.h>      /* printf */
+#include <iostream>     /* cout */
+#include <time.h>       /* time_t, struct tm, difftime, time, mktime */
+#include <vector>
+#include <omp.h>        /* OpenMP */
+
+using namespace std;
+
+int* merge(int *xs, int *ys);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // creating an array of random ints with length 1000000
+    int lenRandArr = 1000000;
+    int *randArr = new int[lenRandArr];
+    
+    vector<int> randList;
+    
+    srand((unsigned int)time(NULL));
+    unsigned long t = clock();
+    cout<<"creating an array of random ints with length "<<lenRandArr<<"\n";
+    for (int i=0; i<lenRandArr; i++){
+        randList.pushrand();
+    }
+    
+    float seconds = ((float)(clock() - t))/CLOCKS_PER_SEC;
+    cout<<"time taken to create array sequentially: "<< seconds <<" seconds\n";
+    
+    // finding max in array sequentially
+    int max = INT_MIN;
+    
+    t = clock();
+    for (int i=0; i<lenRandArr; i++){
+        if(max< randArr[i]) max = randArr[i];
+    }
+    seconds = ((float)(clock() - t))/CLOCKS_PER_SEC;
+    
+    cout<<"max is: "<<max<<", sequential time: "<< seconds <<" seconds\n";
+    
+    // finding max in array parallel
+    max = INT_MIN;
+    
+    t = clock();
+    #pragma omp parallel
+    #pragma omp for
+    for (int i=0; i<lenRandArr; i++){
+        if(max< randArr[i]) max = randArr[i];
+    }
+    seconds = ((float)(clock() - t))/CLOCKS_PER_SEC;
+    
+    cout<<"max is: "<<max<<", parallel time: "<< seconds <<" seconds\n";
+    
+    
     return 0;
+}
+
+int* merge(int *xs, int *ys){
+    
 }
